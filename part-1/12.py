5d9a8f8262d923c8571381e4b328a98cb302de28
Muhlex <21311428+Muhlex@users.noreply.github.com>
2023-05-13 16:30:34 +0200

    Exercise 13

--- part-1/12.py
+++ part-1/12.py
@@ -8,7 +8,7 @@ echo = Pin(18, Pin.IN)
 speaker = PWM(Pin(16, Pin.OUT))
 
 def mapRange(value, inMin, inMax, outMin, outMax):
-  return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
+    return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin
 
 def measureCM():
     trigger.low()
